4c31d2ca2c7d1bca08ca77c1fb63e34468d632cb
HasiniMuvva <muvva.h@northeastern.edu>
2024-02-21 18:06:10 -0500

    commiting changes for assignment4

--- variables.tf
+++ variables.tf
@@ -1,15 +1,44 @@
 variable "project_id" {
   description = "GCP Project ID"
+  type= string
 }
 
 variable "region" {
   description = "GCP Region"
+  type= string
 }
 
 variable "zone" {
   description = "GCP Zone"
+  type= string
 }
 
 variable "next_hop" {
   description = "Next hop for route"
+  type= string
+}
+
+variable "imagename" {
+  description = "give image name"
+  type= string
+}
+
+variable "webapp_ip" {
+  description = "subnet cidr"
+  type= string
+}
+
+variable "db_ip" {
+  description = "subnet cidr"
+  type= string
+}
+
+variable "routerange" {
+  description = "route range"
+  type= string
+}
+
+variable "reg" {
+  description = "route mode"
+  type= string
 }
