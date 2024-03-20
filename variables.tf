e09d750d7f5d4c0d9ac1b61123648e422ac8dcaf
HasiniMuvva <muvva.h@northeastern.edu>
2024-03-20 05:18:55 -0400

    assignment6 commit

--- variables.tf
+++ variables.tf
@@ -1,48 +1,44 @@
 variable "project_id" {
   description = "GCP Project ID"
-  type= string
+  type        = string
 }
 
 variable "region" {
   description = "GCP Region"
-  type= string
+  type        = string
 }
 
-variable "mykeystored" {
-  description = "my key for service account"
-  type= string
-}
 variable "zone" {
   description = "GCP Zone"
-  type= string
+  type        = string
 }
 
 variable "next_hop" {
   description = "Next hop for route"
-  type= string
+  type        = string
 }
 
 variable "imagename" {
   description = "give image name"
-  type= string
+  type        = string
 }
 
 variable "webapp_ip" {
   description = "subnet cidr"
-  type= string
+  type        = string
 }
 
 variable "db_ip" {
   description = "subnet cidr"
-  type= string
+  type        = string
 }
 
 variable "routerange" {
   description = "route range"
-  type= string
+  type        = string
 }
 
 variable "reg" {
   description = "route mode"
-  type= string
+  type        = string
 }
