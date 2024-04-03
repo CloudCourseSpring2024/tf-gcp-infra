41e6c0afeace69ff399655273ccdfce749190d3e
HasiniMuvva <muvva.h@northeastern.edu>
2024-04-03 01:30:28 -0400

    assignment8 added autoscaler and load balancer

--- main.tf
+++ main.tf
@@ -17,11 +17,177 @@ provider "google" {
 data "google_project" "project" {
   project_id = var.project_id
 }
-data "google_compute_instance" "assign6_instance" {
-  name = google_compute_instance.web_instance.name
-  zone = var.zone
+# data "google_compute_instance" "assign6_instance" {
+#   name = google_compute_instance.web_instance.name
+#   zone = var.zone
+# }
+resource "google_compute_region_instance_template" "instance_template" {
+  name         = "instance-template"
+  machine_type = "e2-small"
+  region       = var.region
+  tags         = ["webapp-lb-target", "ssh-access","application-instance"]
+
+  disk {
+    source_image = var.imagename
+    auto_delete  = true
+    boot         = true
+  }
+   service_account {
+    email  = google_service_account.cdn_service_account.email
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+  network_interface {
+    network  = google_compute_network.vpc-tf.self_link
+    subnetwork = google_compute_subnetwork.webapp.self_link
+  }
+  metadata = {
+    startup-script = <<-EOF
+      ClientAliveInterval 60
+      ClientAliveCountMax 3
+      #!/bin/bash
+      mkdir -p /opt/csye6225
+      chown csye6225:csye6225 /opt/csye6225
+
+      cat <<-EOL > /opt/csye6225/.env
+      DB_DIALECT=mysql
+      DB_HOST=${google_sql_database_instance.instance.private_ip_address}
+      DB_PORT=3306
+      DB_USERNAME=webapp
+      DB_PASSWORD=${google_sql_user.webapp.password}
+      DB_NAME=webapp
+      MAILGUN_API_KEY=${var.mailgun_api_key}
+      MAILGUN_DOMAIN=${var.mailgun_domain}
+      EOL
+      
+      cd
+      sudo systemctl daemon-reload
+      sudo systemctl enable nodeindex.service
+      sudo systemctl restart nodeindex.service
+      EOF
+    }
+     depends_on = [google_sql_database_instance.instance, google_sql_user.webapp]
 }
 
+ 
+# Create compute health check
+resource "google_compute_health_check" "health_check" {
+  name               = "health-check"
+  check_interval_sec = 10
+  timeout_sec        = 5
+  http_health_check {
+    request_path = "/healthz"
+    port = 3000
+  }
+}
+
+resource "google_compute_region_instance_group_manager" "instance_group_manager" {
+  name               = "instance-group-manager"
+  base_instance_name = "web-instance"
+  region = var.region
+  version {
+    instance_template = google_compute_region_instance_template.instance_template.id
+    name              = "primary"
+  }
+    named_port {
+    name = "http"
+    //This name given to the 'named_port' in the MIG must match the 'port_name' in the 'google_compute_backend_service'
+    port = "3000"
+  }
+   update_policy {
+    type = "PROACTIVE"
+    //MIG 'proactively' executes actions in order to bring instances to their target template version
+    instance_redistribution_type = "PROACTIVE"
+    //MIG attempts to maintain an even distribution of VM instances across all the zones in the region
+    minimal_action = "REPLACE"
+    most_disruptive_allowed_action = "REPLACE"
+    max_surge_fixed = 3
+  }
+   auto_healing_policies {
+    health_check      = google_compute_health_check.health_check.self_link
+    initial_delay_sec = 300
+  }
+
+}
+# resource "google_compute_target_pool" "default" {
+#   name = "my-target-pool"
+# }
+# Create compute autoscaler
+resource "google_compute_region_autoscaler" "autoscaler" {
+  name        = "autoscaler-auto"
+  project = var.project_id
+  region = var.region
+  target      = google_compute_region_instance_group_manager.instance_group_manager.id
+  autoscaling_policy {
+    min_replicas = 1
+    max_replicas = 3
+    cpu_utilization {
+      target = 0.05
+    }
+  }
+  depends_on = [google_compute_region_instance_group_manager.instance_group_manager]
+}
+
+# Update firewall ingress rules
+resource "google_compute_firewall" "lb_firewall" {
+  name    = "firewall-lb"
+  network = google_compute_network.vpc-tf.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80","443"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["webapp-lb-target","application-instance"]
+}
+resource "google_compute_global_address" "lb_ip" {
+  name          = "lb-ip"
+  ip_version    = "IPV4"
+}
+resource "google_compute_url_map" "url_map" {
+  name            = "url-map"
+  default_service = google_compute_backend_service.backend_service.self_link
+}
+
+resource "google_compute_managed_ssl_certificate" "ssl_certificate" {
+  project     = var.project_id
+  name        = "ssl-certificate"
+  managed {
+    domains = ["spring2024cc.me"]
+  }
+}
+# Define the target HTTPS proxy
+resource "google_compute_target_https_proxy" "lb_https_proxy" {
+  name             = "lb-https-proxy"
+  url_map          = google_compute_url_map.url_map.self_link
+  ssl_certificates = [google_compute_managed_ssl_certificate.ssl_certificate.self_link]
+}
+
+# Define the global forwarding rule
+resource "google_compute_global_forwarding_rule" "lb_forwarding_rule" {
+  name       = "lb-forwarding-rule"
+  ip_protocol = "TCP"
+  port_range = 443
+  target     = google_compute_target_https_proxy.lb_https_proxy.self_link
+  ip_address    = google_compute_global_address.lb_ip.address
+}
+# Create external load balancer
+resource "google_compute_backend_service" "backend_service" {
+  name                    = "backend-service"
+  health_checks           = [google_compute_health_check.health_check.id]
+  protocol                = "HTTP"
+  port_name               = "http"
+  timeout_sec             = 10
+  //enable_cdn              = false
+   backend {
+    group = google_compute_region_instance_group_manager.instance_group_manager.instance_group
+  }
+}
+
+resource "google_project_iam_member" "lb_admin" {
+  project = var.project_id
+  role    = "roles/compute.loadBalancerAdmin"
+  member = "serviceAccount:${google_service_account.cdn_service_account.email}"
+}
 resource "google_dns_record_set" "spring2024Cloud" {
   name         = "spring2024cc.me."
   type         = "A"
@@ -29,26 +195,30 @@ resource "google_dns_record_set" "spring2024Cloud" {
   managed_zone = "spring2024cc"
   
   rrdatas = [
-    data.google_compute_instance.assign6_instance.network_interface[0].access_config[0].nat_ip,
+    google_compute_global_address.lb_ip.address
   ]
 }
-
-resource "google_service_account" "service_account_vm" {
-  account_id   = "my-service-account-vm"
-  display_name = "My Service Account for vm"
+resource "google_project_iam_member" "dns_admin" {
+  project = var.project_id
+  role    = "roles/dns.admin"
+  member = "serviceAccount:${google_service_account.cdn_service_account.email}"
+  
 }
-
+# resource "google_service_account" "service_account_vm" {
+#   account_id   = "my-service-account-vm"
+#   display_name = "My Service Account for vm"
+# }
 # Bind IAM roles to the service account
 resource "google_project_iam_binding" "service_account_logging_admin" {
   project = var.project_id
   role    = "roles/logging.admin"
-  members = ["serviceAccount:${google_service_account.service_account_vm.email}"]
+  members = ["serviceAccount:${google_service_account.cdn_service_account.email}"]
 }
 
 resource "google_project_iam_binding" "service_account_metric_writer" {
   project = var.project_id
   role    = "roles/monitoring.metricWriter"
-  members = ["serviceAccount:${google_service_account.service_account_vm.email}"]
+  members = ["serviceAccount:${google_service_account.cdn_service_account.email}"]
 }
 
 resource "google_compute_network" "vpc-tf" {
@@ -172,58 +342,54 @@ resource "google_compute_firewall" "allow_application_port" {
 resource "google_project_iam_binding" "service_account_pubsub_publisher" {
   project = var.project_id
   role    = "roles/pubsub.publisher"
-  members = ["serviceAccount:${google_service_account.service_account_vm.email}"]
+  members = ["serviceAccount:${google_service_account.cdn_service_account.email}"]
 }
 
-resource "google_compute_instance" "web_instance" {
-  name         = "web-instance"
-  machine_type = "e2-small"
-  zone         = var.zone
-  tags         = ["application-instance"]
+# resource "google_compute_instance" "web_instance" {
+#   name         = "web-instance"
+#   machine_type = "e2-small"
+#   zone         = var.zone
+#   tags         = ["application-instance"]
+#   boot_disk {
+#     initialize_params {
+#       image = var.imagename
+#       type  = "pd-balanced"
+#       size  = 100
+#     }
+#   }
+#   service_account {
+#     email  = google_service_account.service_account_vm.email
+#     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+#   }
+#   network_interface {
+#     subnetwork = google_compute_subnetwork.webapp.self_link
+#     access_config {}
+#   }
+  # metadata = {
+  #   startup-script = <<-EOF
+  #     #!/bin/bash
+  #     mkdir -p /opt/csye6225
+  #     chown csye6225:csye6225 /opt/csye6225
 
-  boot_disk {
-    initialize_params {
-      image = var.imagename
-      type  = "pd-balanced"
-      size  = 100
-    }
-  }
-
-  service_account {
-    email  = google_service_account.service_account_vm.email
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
-
-  network_interface {
-    subnetwork = google_compute_subnetwork.webapp.self_link
-    access_config {}
-  }
-
-  metadata = {
-    startup-script = <<-EOF
-      #!/bin/bash
-      mkdir -p /opt/csye6225
-      chown csye6225:csye6225 /opt/csye6225
-
-      cat <<-EOL > /opt/csye6225/.env
-      DB_DIALECT=mysql
-      DB_HOST=${google_sql_database_instance.instance.private_ip_address}
-      DB_PORT=3306
-      DB_USERNAME=webapp
-      DB_PASSWORD=${google_sql_user.webapp.password}
-      DB_NAME=webapp
-      MAILGUN_API_KEY=${var.mailgun_api_key}
-      MAILGUN_DOMAIN=${var.mailgun_domain}
-      EOL
+  #     cat <<-EOL > /opt/csye6225/.env
+  #     DB_DIALECT=mysql
+  #     DB_HOST=${google_sql_database_instance.instance.private_ip_address}
+  #     DB_PORT=3306
+  #     DB_USERNAME=webapp
+  #     DB_PASSWORD=${google_sql_user.webapp.password}
+  #     DB_NAME=webapp
+  #     MAILGUN_API_KEY=${var.mailgun_api_key}
+  #     MAILGUN_DOMAIN=${var.mailgun_domain}
+  #     EOL
       
-      cd
-      sudo systemctl daemon-reload
-      sudo systemctl enable nodeindex.service
-      sudo systemctl restart nodeindex.service
-      EOF
-    }
-     depends_on = [google_sql_database_instance.instance, google_sql_user.webapp]
-}
+  #     cd
+  #     sudo systemctl daemon-reload
+  #     sudo systemctl enable nodeindex.service
+  #     sudo systemctl restart nodeindex.service
+  #     EOF
+  #   }
+  #    depends_on = [google_sql_database_instance.instance, google_sql_user.webapp]
+# }
 data "google_dns_managed_zone" "spring2024cc" {
   name = "spring2024cc"
 }
@@ -232,10 +398,10 @@ resource "google_dns_record_set" "spf_record" {
   managed_zone = data.google_dns_managed_zone.spring2024cc.name
   name         = "mg.spring2024cc.me."
   type         = "TXT"
-  ttl          = 3600
+  ttl          = 300
   
   rrdatas = [
-    "v=spf1 include:mailgun.org ~all"
+    google_compute_global_forwarding_rule.lb_forwarding_rule.ip_address,
   ]
 }
 
@@ -243,7 +409,7 @@ resource "google_dns_record_set" "dkim_record" {
   managed_zone = data.google_dns_managed_zone.spring2024cc.name
   name         = "krs._domainkey.mg.spring2024cc.me."
   type         = "TXT"
-  ttl          = 3600
+  ttl          = 300
 
   rrdatas = [
     "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDOS24Ono1/EaJ0oms0qkY1z5ZMKLXojpiWnnQpfABLiK6Kk/vdcreWsreu+lUSQ21Bu0KK+lsPW/1WqSH/yEVw+tA3L3hJe4btB1/P/E9MuEf6qbDACVKY1+/PQr/P6wVX098jWDa0u6xImT//IHzxkRrYzrNtxtIWqdlDejAk9wIDAQAB"
@@ -254,7 +420,7 @@ resource "google_dns_record_set" "mx_records" {
   managed_zone = data.google_dns_managed_zone.spring2024cc.name
   name         = "mg.spring2024cc.me."
   type         = "MX"
-  ttl          = 3600
+  ttl          = 300
 
   rrdatas = [
     "10 mxa.mailgun.org.",
@@ -266,7 +432,7 @@ resource "google_dns_record_set" "cname_record" {
   managed_zone = data.google_dns_managed_zone.spring2024cc.name
   name         = "email.mg.spring2024cc.me."
   type         = "CNAME"
-  ttl          = 3600
+  ttl          = 300
 
   rrdatas = [
     "mailgun.org."
@@ -327,8 +493,6 @@ resource "google_vpc_access_connector" "serverless_connector" {
   network       = google_compute_network.vpc-tf.id
   ip_cidr_range = "10.0.5.0/28" # Choose a range that does not overlap with existing subnets.
 }
-
-
 
 resource "google_project_iam_member" "cloud_sql_client" {
   project = var.project_id
@@ -431,9 +595,6 @@ resource "google_project_iam_member" "service_agent" {
   member  = "serviceAccount:service-${data.google_project.project.number}@gcf-admin-robot.iam.gserviceaccount.com"
 }
 
-
-
-
 output "cloud_function_name" {
   value = google_cloudfunctions2_function.email_verification.name
 }
@@ -442,33 +603,7 @@ output "cloud_function_pubsub_topic" {
   value = google_pubsub_topic.verify_email.name
 }
 
-
-
-
-# # IAM policy for Cloud Functions CloudFunction
-# data "google_iam_policy" "cf" {
-#   binding {
-#     role = "roles/viewer"
-
-#     members = [
-#       "serviceAccount:${google_service_account.cdn_service_account.email}",
-#     ]
-#   }
-# }
-
-# resource "google_cloudfunctions_function_iam_policy" "function_iam_policy" {
-#   project        = var.project
-#   region         = var.region
-#   cloud_function = google_cloudfunctions2_function.email_verification.name
-
-#   policy_data = data.google_iam_policy.cf.policy_data
-#   depends_on = [google_cloudfunctions2_function.email_verification]
-# }
-
-
-
 # # IAM policy for Pub/Sub Subscription
-
 data "google_iam_policy" "subscriber" {
   binding {
     role = "roles/editor"
@@ -485,36 +620,30 @@ resource "google_pubsub_subscription_iam_policy" "subscription_iam_policy" {
   policy_data = data.google_iam_policy.subscriber.policy_data
 }
 
-
-
-////////
-
 # # IAM policy for Cloud Pub/Sub Topic
 
 data "google_iam_policy" "admin" {
   binding {
-    role = "roles/viewer"
+    role = "roles/editor"
 
     members = [
       "serviceAccount:${google_service_account.cdn_service_account.email}",
     ]
   }
 }
+
+
 resource "google_pubsub_topic_iam_policy" "topic_iam_policy" {
   topic   = google_pubsub_topic.verify_email.name
   project = var.project_id
 
   policy_data = data.google_iam_policy.admin.policy_data
 }
+# output "instance_name" {
+#   value = google_compute_instance.web_instance.name
+# }
 
-
-
-# Outputs
-output "instance_name" {
-  value = google_compute_instance.web_instance.name
-}
-
-output "sql_instance_private_ip" {
-  value = google_sql_database_instance.instance.private_ip_address
-}
+# output "sql_instance_private_ip" {
+#   value = google_sql_database_instance.instance.private_ip_address
+# }
    
